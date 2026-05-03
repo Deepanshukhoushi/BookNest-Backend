18b8a9878b7bcf31275e59bb028e1ccd37de1395
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-05-03 19:35:11 +0530

    [Deepanshu]: updated admin server tests

--- admin-server/src/test/java/com/booknest/adminserver/AdminServerApplicationTests.java
+++ admin-server/src/test/java/com/booknest/adminserver/AdminServerApplicationTests.java
@@ -3,7 +3,10 @@ package com.booknest.adminserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.TestPropertySource;
+
 @SpringBootTest
+@TestPropertySource(properties = {"eureka.client.enabled=false"})
 class AdminServerApplicationTests {
 
 	@Test
